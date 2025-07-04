82ceeda512e910448eb68ccdaaca1006bf2dbb6d
Diogo Bento <diogo.bento@ltplabs.com>
2025-07-04 15:57:50 +0100

    try other fix

--- pipelines/pirate.py
+++ pipelines/pirate.py
@@ -7,36 +7,27 @@ from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import BaseTool, tool
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.tools import WikipediaQueryRun
 
-# Define Summation Tool
-class SummationInput(BaseModel):
-    a: float = Field(description="First number.")
-    b: float = Field(description="Second number.")
+from logging import getLogger
+logger = getLogger(__name__)
+logger.setLevel("DEBUG")
 
-@tool("summation", args_schema=SummationInput, return_direct=False)
+
+@tool("summation", return_direct=False)
 def summation(a: float, b: float) -> float:
     """Adds two numbers together."""
     return a + b
 
-# Define Multiplication Tool
-class MultiplicationInput(BaseModel):
-    a: float = Field(description="First number.")
-    b: float = Field(description="Second number.")
 
-@tool("multiplication", args_schema=MultiplicationInput, return_direct=False)
+@tool("multiplication",return_direct=False)
 def multiplication(a: float, b: float) -> float:
     """Multiplies two numbers together."""
     return a * b
 
-# Import the necessary classes for Wikipedia query tool
-from langchain_community.utilities import WikipediaAPIWrapper
-from langchain_community.tools import WikipediaQueryRun
 
-# Wikipedia Tool
-class WikipediaSearchInput(BaseModel):
-    query: str = Field(description="The query to search Wikipedia for.")
-
-@tool("search_wikipedia", args_schema=WikipediaSearchInput, return_direct=False)
+@tool("search_wikipedia", return_direct=False)
 def search_wikipedia(query: str) -> str:
     """Search Wikipedia for pages related to the query."""
     wikipedia_api = WikipediaAPIWrapper(top_k_results=5)  # Configure for top 5 results
@@ -44,8 +35,14 @@ def search_wikipedia(query: str) -> str:
     results = tool.run(query)  # Execute the search
     return results  # Return the results
 
+
 class Pipeline:
+
     class Valves(BaseModel):
+        # OPENAI_API_KEY: str = Field(default="", description="OpenAI API key")
+        # RATE_LIMIT: int = Field(default=5, description="Rate limit for the pipeline")
+        # WORD_LIMIT: int = Field(default=300, description="Word limit when getting page summary")
+        # WIKIPEDIA_ROOT: str = Field(default="https://en.wikipedia.org/wiki", description="Wikipedia root URL")
         OPENAI_API_BASE_URL: str = ""#os.getenv("URL", "")
         AZURE_OPENAI_API_KEY: str = ""
         OPENAI_API_MODEL: str = "gpt-4o"
@@ -55,35 +52,40 @@ class Pipeline:
         )
 
     def __init__(self):
-        self.name = "Chat with Pirate"
-        self.tools = [summation, multiplication, search_wikipedia]  # Add Wikipedia tool to the list
-        self.valves = self.Valves(
-            #AZURE_OPENAI_API_KEY=os.getenv("AZURE_OPENAI_API_KEY", "")
-        )
-    #     self.pipelines = self.get_openai_models()
+        # Optionally, you can set the id and name of the pipeline.
+        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
+        # The identifier must be unique across all pipelines.
+        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
+        # self.id = "wiki_pipeline"
+        self.name = "Test Pipeline"
 
-    # def get_openai_models(self):
-    #     if self.valves.AZURE_OPENAI_API_KEY:
-    #         try:
-    #             headers = {
-    #                 "Authorization": f"Bearer {self.valves.AZURE_OPENAI_API_KEY}",
-    #                 "Content-Type": "application/json"
-    #             }
-    #             response = requests.get(
-    #                 f"{self.valves.OPENAI_API_BASE_URL}/models", headers=headers
-    #             )
-    #             models = response.json()
-    #             return [
-    #                 {"id": model["id"], "name": model.get("name", model["id"])}
-    #                 for model in models["data"] if "gpt" in model["id"]
-    #             ]
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #             return [{"id": "error", "name": "Could not fetch models from OpenAI."}]
-    #     else:
-    #         return []
+        # Initialize valve paramaters
+        # self.valves = self.Valves(
+        #     **{k: os.getenv(k, v.default) for k, v in self.Valves.model_fields.items()}
+        # )
 
-    def pipe(self, user_message: str, model_id: str, messages: List[dict], body: dict):
+        self.valves = self.Valves()
+
+        self.tools = [summation, multiplication, search_wikipedia]
+
+    async def on_startup(self):
+        # This function is called when the server is started.
+        logger.debug(f"on_startup:{self.name}")
+        pass
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        logger.debug(f"on_shutdown:{self.name}")
+        pass
+
+    def pipe(
+        self, 
+        user_message: str, 
+        model_id: str, 
+        messages: List[dict], 
+        body: dict
+    ):
+        
         try:
             model = AzureChatOpenAI(
                 azure_deployment="gpt-4o",
